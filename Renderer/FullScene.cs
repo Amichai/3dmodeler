6de3339af369458a467b449c0471d1d70e227571
amichai <amichaimlevy@gmail.com>
2015-08-27 08:20:47 -0400

    Refactorization, UI.

---
+++ Renderer/FullScene.cs
@@ -0,0 +1,88 @@
+﻿using Modeler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace Renderer {
+    class FullScene {
+        public FullScene() {
+            // Define the camera
+            PerspectiveCamera camera = new PerspectiveCamera();
+            camera.Position = new Point3D(0, .25, 10);
+            camera.LookDirection = new Vector3D(0, -.05, -1);
+            camera.UpDirection = new Vector3D(0, 1, 0);
+            camera.FieldOfView = 60;
+
+            // Define a lighting model
+            DirectionalLight light = new DirectionalLight();
+            light.Color = Colors.White;
+            light.Direction = new Vector3D(-0.5, -0.25, -0.5);
+
+            // Collect the components
+            this.modelGroup = new Model3DGroup();
+            this.modelGroup.Children.Add(light);
+
+            ModelVisual3D visual = new ModelVisual3D();
+            visual.Content = this.modelGroup;
+            Viewport3D viewport = new Viewport3D();
+            viewport.Children.Add(visual);
+            viewport.Camera = camera;
+
+            this.Viewport = viewport;
+        }
+
+        private Model3DGroup modelGroup;
+        public Viewport3D Viewport { get; private set; }
+        GeometryModel3D geometry;
+
+        public void Add(Model model) {
+            MeshGeometry3D frontMesh = new MeshGeometry3D();
+            frontMesh.Positions = new Point3DCollection(model.Vertices.Select(i => new Point3D(i.X, i.Y, i.Z)));
+            frontMesh.TriangleIndices = new Int32Collection(model.FaceTriangleIndices);
+            this.geometry = new GeometryModel3D();
+            geometry.Geometry = frontMesh;
+            this.modelGroup.Children.Add(geometry);
+
+            // *** Material ***
+            DiffuseMaterial diffTransYellow =
+                new DiffuseMaterial(new SolidColorBrush(Color.FromArgb(64, 255, 255, 0)));
+            SpecularMaterial specTransWhite =
+                new SpecularMaterial(new SolidColorBrush(Color.FromArgb(128, 255, 255, 255)), 30.0);
+            MaterialGroup material = new MaterialGroup();
+            material.Children.Add(diffTransYellow);
+            material.Children.Add(specTransWhite);
+
+            geometry.Material = material;
+        }
+
+        private double lastAngleX = 0, lastAngleY = 0;
+        public void RotateModel(double dx, double dy) {
+            var rotations = new Transform3DCollection();
+            var transformGroup = new Transform3DGroup() {
+                Children = rotations
+            };
+
+            geometry.Transform = transformGroup;
+
+            lastAngleX -= dx / 10;
+            rotations.Add(new RotateTransform3D(
+                new QuaternionRotation3D(
+                    new Quaternion(new Vector3D(0, 1, 0), lastAngleX)
+                    )
+                )
+            );
+            lastAngleY -= dy / 10;
+            rotations.Add(new RotateTransform3D(
+                new QuaternionRotation3D(
+                    new Quaternion(new Vector3D(1, 0, 0), lastAngleY)
+                    )
+                )
+           );
+        }
+    }
+}
--- Renderer/MainWindow.xaml.cs
+++ Renderer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Modeler;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,56 +24,13 @@ namespace Renderer {
     public partial class MainWindow : Window {
         public MainWindow() {
             Model model = Model.Cube();
+            this.scene = new FullScene();
+            scene.Add(model);
             InitializeComponent();
-            this.viewRoot.Children.Add(ToViewport3D(model));
+            this.viewRoot.Children.Add(scene.Viewport);
         }
 
-
-        static Viewport3D ToViewport3D(Model model) {
-            // Define the camera
-            PerspectiveCamera qCamera = new PerspectiveCamera();
-            qCamera.Position = new Point3D(0, .25, 4.25);
-            qCamera.LookDirection = new Vector3D(0, -.05, -1);
-            qCamera.UpDirection = new Vector3D(0, 1, 0);
-            qCamera.FieldOfView = 60;
-
-            // Define a lighting model
-            DirectionalLight qLight = new DirectionalLight();
-            qLight.Color = Colors.Red;    
-            qLight.Direction = new Vector3D(-0.5, -0.25, -0.5);
-
-            MeshGeometry3D qFrontMesh = new MeshGeometry3D();
-            qFrontMesh.Positions = new Point3DCollection(model.Vertices.Select(i => new Point3D(i.X, i.Y, i.Z)));
-            qFrontMesh.TriangleIndices = new Int32Collection(model.FaceTriangleIndices);
-
-            // Outer Tetrahedron (semi-transparent) : Define the mesh, material and transformation.
-            GeometryModel3D qOuterGeometry = new GeometryModel3D();
-            qOuterGeometry.Geometry = qFrontMesh;
-            // *** Material ***
-            DiffuseMaterial qDiffTransYellow =
-                new DiffuseMaterial(new SolidColorBrush(Color.FromArgb(64, 255, 255, 0)));
-            SpecularMaterial qSpecTransWhite =
-                new SpecularMaterial(new SolidColorBrush(Color.FromArgb(128, 255, 255, 255)), 30.0);
-            MaterialGroup qOuterMaterial = new MaterialGroup();
-            qOuterMaterial.Children.Add(qDiffTransYellow);
-            qOuterMaterial.Children.Add(qSpecTransWhite);
-            qOuterGeometry.Material = qOuterMaterial;
-
-            // Collect the components
-            Model3DGroup qModelGroup = new Model3DGroup();
-            qModelGroup.Children.Add(qLight);
-            qModelGroup.Children.Add(qOuterGeometry);
-
-            qOuterGeometry.Transform = new RotateTransform3D(new QuaternionRotation3D(new Quaternion(new Vector3D(1,1,1), 20)));
-            ModelVisual3D qVisual = new ModelVisual3D();
-            qVisual.Content = qModelGroup;
-            Viewport3D qViewport = new Viewport3D();
-            qViewport.Children.Add(qVisual);
-            qViewport.Camera = qCamera;
-
-            return qViewport;
-        }
-
+        private FullScene scene;
 
         private bool dragging = false;
         private Point lastMousePosition;
@@ -89,8 +47,8 @@ namespace Renderer {
             if (this.dragging) {
                 var currentPosition = e.GetPosition(this.viewRoot);
                 var diff = this.lastMousePosition - currentPosition;
-
                 this.lastMousePosition = currentPosition;
+                this.scene.RotateModel(diff.X, diff.Y);
             }
         }
     }
